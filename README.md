eeb32f38c226cf1f8a22084dff8472e980f8642a
Antony <itsokaybehappy@gmail.com>
2023-04-20 17:20:24 -0400

    v.02

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 v.01
 - added Navbar and Footer formatting
+
+v.02
+- added logo
+- added phone icon and format
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,32 @@
+import Image from "next/image"
+import styles from "../styles/Navbar.module.css"
+import logo from "../public/img/logo.png"
+import {AiOutlinePhone} from "react-icons/ai"
+import Head from "next/head"
+
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className ={styles.container}>
+        <div className ={styles.item}>
+          <div className={styles.phone}>
+            <AiOutlinePhone/>
+          </div>
+           <div className = {styles.texts}>
+            <div className={styles.text}>
+              302-292-8982
+            </div>
+            <div className ={styles.text}>
+              Order by phone or online!
+            </div>
+           </div>
+        </div>
+        <div className ={styles.item}>
+            <Image src ={logo} alt ="" />
+        </div>
+        <div className ={styles.item}>
+            right
+        </div>
+    
     </div>
   )
 }
